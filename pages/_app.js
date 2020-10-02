284c43e68a691ecd306cef04fddab0fb955ab484
madebymak <mr.jmak@gmail.com>
2020-10-02 16:23:31 -0700

    Add resource picker

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import { Provider } from '@shopify/app-bridge-react';
 import '@shopify/polaris/dist/styles.css';
 import translations from '@shopify/polaris/locales/en.json';
 import Cookies from 'js-cookie';
+import ClientRouter from '../components/ClientRouter';
 
 class MyApp extends App {
     render() {
@@ -18,6 +19,7 @@ class MyApp extends App {
                     <meta charSet="utf-8" />
                 </Head>
                 <Provider config={config}>
+                    <ClientRouter />
                     <AppProvider i18n={ translations }>
                         <Component {...pageProps } />
                     </AppProvider>
