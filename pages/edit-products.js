b365392afa018a7b2c2cf4fb125c86e8642ef148
madebymak <mr.jmak@gmail.com>
2020-10-08 15:57:52 -0700

    Add mutations

--- pages/edit-products.js
+++ pages/edit-products.js
@@ -1,12 +1,29 @@
 import { from } from "apollo-boost";
-import { Card, DisplayText, Form, FormLayout, Layout, Page, PageActions, TextField } from '@shopify/polaris';
+import { Banner, Card, DisplayText, Form, FormLayout, Frame, Layout, Page, PageActions, TextField, Toast } from '@shopify/polaris';
 import store from 'store-js';
+import gql from 'graphql-tag';
+import { Mutation} from 'react-apollo';
+
+const UPDATE_PRICE = gql`
+    mutation productVariantUpdate($input: ProductVariantInput!) {
+        productVariantUpdate(input: $input) {
+            product {
+                title
+            }
+            productVariant {
+                id
+                price
+            }
+        }
+    }
+`;
 
 class EditProduct extends React.Component {
     state = {
         discount: '',
         price: '',
-        variantId: ''
+        variantId: '',
+        showToast: false,
     };
 
     componentDidMount() {
@@ -15,52 +32,79 @@ class EditProduct extends React.Component {
 
     render() {
         const { name, price, discount, variantId } = this.state;
-
         return (
-            <Page>
-                <Layout>
-                    <Layout.Section>
-                        <DisplayText size="large">{name}</DisplayText>
-                        <Form>
-                            <Card sectioned>
-                                <FormLayout>
-                                    <FormLayout.Group>
-                                        <TextField
-                                            prefix="$"
-                                            value={price}
-                                            disabled={true}
-                                            label="Original Price"
-                                            type="price" />
-                                        <TextField
-                                            prefix="$"
-                                            value={discount}
-                                            onChange={this.handleChange('discount')}
-                                            label="Discounted Price"
-                                            type="discount" />
-                                    </FormLayout.Group>
-                                    <p> This sale price will expire in two weeks</p>
-                                </FormLayout>
-                            </Card>
-                            <PageActions 
-                                primaryAction={[
-                                    {
-                                        content: 'Save',
-                                        onAction: () => {
-                                            console.log('submitted');
-                                        }
-                                    }
-                                ]}
+            <Mutation mutation={UPDATE_PRICE}>
+                {(handleSubmit, {error, data}) => {
+                    const showError = error && (
+                        <Banner status="critical">{error.message}</Banner>
+                    );
+                    const showToast = data && data.productVariantUpdate && (
+                        <Toast
+                            content="Successfully updated"
+                            onDismiss={() => this.setState({ showToast: false})}
+                        />
+                    );
+                    return (
+                        <Frame>
+                            <Page>
+                                <Layout>
+                                    {showToast}
+                                    <Layout.Section>
+                                        {showError}
+                                    </Layout.Section>
+                                    <Layout.Section>
+                                        <DisplayText size="large">{name}</DisplayText>
+                                        <Form>
+                                            <Card sectioned>
+                                                <FormLayout>
+                                                    <FormLayout.Group>
+                                                        <TextField
+                                                            prefix="$"
+                                                            value={price}
+                                                            disabled={true}
+                                                            label="Original Price"
+                                                            type="price" />
+                                                        <TextField
+                                                            prefix="$"
+                                                            value={discount}
+                                                            onChange={this.handleChange('discount')}
+                                                            label="Discounted Price"
+                                                            type="discount" />
+                                                    </FormLayout.Group>
+                                                    <p> This sale price will expire in two weeks</p>
+                                                </FormLayout>
+                                            </Card>
+                                            <PageActions
+                                                primaryAction={[
+                                                    {
+                                                        content: 'Save',
+                                                        onAction: () => {
+                                                            const productVariableInput = {
+                                                                id: variants,
+                                                                price: discount,
+                                                            };
 
-                                secondaryActions={[
-                                    {
-                                        content: 'Remove discount'
-                                    }
-                                ]}
-                            />
-                        </Form>
-                    </Layout.Section>
-                </Layout>
-            </Page>
+                                                            handleSubmit({
+                                                                variables: { input: productVariableInput },
+                                                            });
+                                                        }
+                                                    }
+                                                ]}
+
+                                                secondaryActions={[
+                                                    {
+                                                        content: 'Remove discount'
+                                                    }
+                                                ]}
+                                            />
+                                        </Form>
+                                    </Layout.Section>
+                                </Layout>
+                            </Page>
+                        </Frame>
+                    );
+                }}
+            </Mutation>
         );
     }
 
